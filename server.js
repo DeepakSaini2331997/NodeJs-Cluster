dc42a7f3a29ba4e48a4476f3a9a9f4f6bd1d5514
deepak <deepaksaini2331997@gmail.com>
2025-01-18 14:08:04 +0530

    cluster to handle

--- server.js
+++ server.js
@@ -5,8 +5,22 @@ import cluster from "cluster";
 
 const port = process.env.PORT || 3000
 
-console.log("core cpu count",os.cpus().length)
+//console.log("core cpu count",os.cpus().length)
+//console.log(cluster," my cluster")
 
-app.listen(port,()=>{
-    console.log(process.pid,' connect succefully on port:',port)
-})
+if(cluster.isMaster){
+    console.log('cluter true')
+    const numsCpu = os.cpus().length
+    for(let i=0;i<numsCpu;i++){
+        cluster.fork()
+        //console.log(`for loop ${process.pid}`)
+    }
+    cluster.on('exit',(Worker)=>{
+        console.log(`my worker ${Worker.process.pid}`)
+    })
+}else{
+    app.listen(port,()=>{
+        console.log(process.pid,' connect succefully on port:',port)
+    })
+}
+
